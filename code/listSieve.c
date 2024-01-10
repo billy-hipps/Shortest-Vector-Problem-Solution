9edd483a8419e59c8dd00227d650ebced21565c5
William Hipperson <billyhipperson@hotmail.co.uk>
2024-01-10 15:38:15 +0000

    broken listSieve

--- code/listSieve.c
+++ code/listSieve.c
@@ -13,14 +13,14 @@ double listSieve(double** basis, double** L, double mu, long int nSamples) {
     double delta = 1 - (1 / dim);
     int c = 6;
     long k = (pow(2, (c * dim)));
-    double* v;
     double* vReduced = NULL;
-    for (; nSamples <= k; nSamples++) {
-        v = (double *)calloc(dim, sizeof(double));
-        if (v == NULL) {
-            printf("Memory allocation failed.\n");
-            exit(EXIT_FAILURE);
-        }
+    double*v = (double *)calloc(dim, sizeof(double));
+    if (v == NULL) {
+        printf("MEMORY ERROR: Error allocating memory.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (; nSamples < k; nSamples++) {
+        //printf("nSamples: %ld\n", nSamples);
         // Sample a vector and reduce it
         int sStatus = sample(basis, v);
         if (sStatus != 0) {
@@ -29,54 +29,33 @@ double listSieve(double** basis, double** L, double mu, long int nSamples) {
         }
         vReduced = reduce(v, L, delta, nSamples);
         for (long int j = 0; j < nSamples; j++) {
+            //printf("J: %ld\n", j);
             double* diff = vec_diff(L[j], vReduced);
             double lenDiff = L2_norm(diff);
             int repeatFlag = isIn(vReduced, L, nSamples);
             // Check if v is in L
             if (repeatFlag == 0) {
-                //printf("no repeat\n");
                 // Check if v can be reduced by a vector in L
                 if ((lenDiff < mu) && (lenDiff > 0)) {
                     free(v);
                     v = NULL;
                     free(diff);
                     diff = NULL;
-                    //printf("recursing\n");
                     printf("mu = %lf\n", lenDiff);
                     return listSieve(basis, L, lenDiff, nSamples);
                 } else {
                     // If v cannot be reduced, add it to L
-                    size_t newSize = nSamples + 1;
-                    double **temp = (double **)realloc(L, newSize * sizeof(double *));
-                    if (temp == NULL) {
-                        printf("Memory reallocation failed.\n");
-                        exit(EXIT_FAILURE);
-                    }
-                    L = temp;
-                    L[nSamples] = (double *)malloc(dim * sizeof(double));
-                    if (L[nSamples] == NULL) {
-                        printf("Memory allocation failed.\n");
-                        exit(EXIT_FAILURE);
-                    }
+
                     memcpy(L[nSamples], vReduced, dim * sizeof(double));
-                    free(v);
-                    v = NULL;
-                    free(diff); 
-                    diff = NULL;
-                    break;
+                    memset(v, 0, dim *sizeof(double));
                 }
             } else {
                 free(diff);
                 diff = NULL;
             }
         } 
-        if (nSamples == k) {
-            free(v);
-            v = NULL;
-            break;
-        } else {
-            continue;
-        }
     }
+    free(v);
+    v = NULL;
     return mu;
 }
